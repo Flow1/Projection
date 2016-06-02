a3ef83bbe567ed71f08e7bab640144b4c53b4d70
unknown <thex@thex-outdoor.nl>
2016-06-02 17:43:10 +0200

    c

--- src/org/gavaghan/geodesy/example/Example.java
+++ src/org/gavaghan/geodesy/example/Example.java
@@ -7,15 +7,13 @@ import org.gavaghan.geodesy.Ellipsoid;
 import org.gavaghan.geodesy.GeodeticCalculator;
 import org.gavaghan.geodesy.GlobalCoordinates;
 
-public class Example
-{
+public class Example {
 	/**
 	 * Calculate projection in degrees and meters
-	 *   
+	 * 
 	 * using WGS84 reference ellipsoid
 	 */
-	public void calculation()
-	{
+	public void calculation() {
 		// instantiate the calculator
 		GeodeticCalculator geoCalc = new GeodeticCalculator();
 
@@ -24,43 +22,48 @@ public class Example
 
 		// set Eiffel Tower coordinates
 		GlobalCoordinates eiffelTower;
-		
+
 		double xpos = 48.85889;
 		double ypos = 2.29583;
-		
+
 		eiffelTower = new GlobalCoordinates(xpos, ypos);
 
-
-		double x=5000;
+		double x = 5000;
 		double y = 0;
-		double deg = Math.atan2(x, y)*180.0/Math.PI;
+		double deg = Math.atan2(x, y) * 180.0 / Math.PI;
 		System.out.println(deg);
-		
-		x=0;y=5000;
-		deg = Math.atan2(x, y)*180.0/Math.PI;
+
+		x = 0;
+		y = 5000;
+		deg = Math.atan2(x, y) * 180.0 / Math.PI;
 		System.out.println(deg);
-		
-		x=-5000;y=0;
-		deg = Math.atan2(x, y)*180.0/Math.PI;
-		System.out.println(deg);	
-		
-		x=0;y=-5000;
-		deg = Math.atan2(x, y)*180.0/Math.PI;
+
+		x = -5000;
+		y = 0;
+		deg = Math.atan2(x, y) * 180.0 / Math.PI;
 		System.out.println(deg);
-		
-		x=3267;y=-4834;
-		double angle = Math.atan2(x, y)*180.0/Math.PI;	
-		double dist=Math.sqrt(x*x+y*y);
-		
+
+		x = 0;
+		y = -5000;
+		deg = Math.atan2(x, y) * 180.0 / Math.PI;
+		System.out.println(deg);
+
+		x = 3267;
+		y = -4834;
+		double angle = Math.atan2(x, y) * 180.0 / Math.PI;
+		double dist = Math.sqrt(x * x + y * y);
+
 		// calculate the geodetic curve
-		GlobalCoordinates projected = geoCalc.calculateEndingGlobalCoordinates(reference, eiffelTower, angle, dist);
-	    System.out.printf("   Destination: %1.4f%s", projected.getLatitude(), (projected.getLatitude() > 0) ? "N" : "S" );
-	    System.out.printf(", %1.4f%s\n", projected.getLongitude(), (projected.getLongitude() > 0) ? "E" : "W");	
+		GlobalCoordinates projected = geoCalc.calculateEndingGlobalCoordinates(
+				reference, eiffelTower, angle, dist);
+		System.out.printf("   Destination: %1.4f%s", projected.getLatitude(),
+				(projected.getLatitude() > 0) ? "N" : "S");
+		System.out.printf(", %1.4f%s\n", projected.getLongitude(),
+				(projected.getLongitude() > 0) ? "E" : "W");
 	}
 
-	static public void main(String[] args)
-	{
-	    Example t = new Example();
-	    t.calculation();
+	static public void main(String[] args) {
+		Example t = new Example();
+		t.calculation();
 	}
 }
