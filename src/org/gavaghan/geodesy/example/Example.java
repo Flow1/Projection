bfb410fd94e0018f6de086f7ac461f06b499f6d7
unknown <thex@thex-outdoor.nl>
2016-06-06 19:59:25 +0200

    Real position check

--- src/org/gavaghan/geodesy/example/Example.java
+++ src/org/gavaghan/geodesy/example/Example.java
@@ -9,7 +9,7 @@ import org.gavaghan.geodesy.GlobalCoordinates;
 
 public class Example {
 	/**
-	 * Calculate projection in degrees and meters
+	 * Calculate projection in degrees and radar meters
 	 * 
 	 * using WGS84 reference ellipsoid
 	 */
@@ -20,42 +20,38 @@ public class Example {
 		// select a reference elllipsoid
 		Ellipsoid reference = Ellipsoid.WGS84;
 
-		// set Eiffel Tower coordinates
-		GlobalCoordinates eiffelTower;
+		// set virtual radar coordinates
+		GlobalCoordinates virtualRadar;
 
-		double xpos = 48.85889;
-		double ypos = 2.29583;
+		// Virtual radar position Scheldt Area
+		double xpos = 51.41667;
+		double ypos = 4.08333;
 
-		eiffelTower = new GlobalCoordinates(xpos, ypos);
+		//OL: 4º 05' 00", NB: 51º 25' 00"
+		//N 51 25.000 E004 05.000
+		//N 51.41667, E 004.08333
+		//RD: 64301, 381648
+		virtualRadar = new GlobalCoordinates(xpos, ypos);
 
+		// Radar coordinate
 		double x = 5000;
 		double y = 0;
-		double deg = Math.atan2(x, y) * 180.0 / Math.PI;
-		System.out.println(deg);
-
-		x = 0;
-		y = 5000;
-		deg = Math.atan2(x, y) * 180.0 / Math.PI;
-		System.out.println(deg);
-
-		x = -5000;
-		y = 0;
-		deg = Math.atan2(x, y) * 180.0 / Math.PI;
-		System.out.println(deg);
-
-		x = 0;
-		y = -5000;
-		deg = Math.atan2(x, y) * 180.0 / Math.PI;
-		System.out.println(deg);
-
-		x = 3267;
-		y = -4834;
+		// 9.7674 km distance
+		
+		// Conversion into meters (according to spec)
+		double bit = 2000.0/1024.0;
+		x=x*bit;
+		y=y*bit;
+		
+		// Triangulate into angle and distance
 		double angle = Math.atan2(x, y) * 180.0 / Math.PI;
 		double dist = Math.sqrt(x * x + y * y);
 
-		// calculate the geodetic curve
+		// And project the geodetic curve
 		GlobalCoordinates projected = geoCalc.calculateEndingGlobalCoordinates(
-				reference, eiffelTower, angle, dist);
+				reference, virtualRadar, angle, dist);
+		
+		// Output
 		System.out.printf("   Destination: %1.4f%s", projected.getLatitude(),
 				(projected.getLatitude() > 0) ? "N" : "S");
 		System.out.printf(", %1.4f%s\n", projected.getLongitude(),
